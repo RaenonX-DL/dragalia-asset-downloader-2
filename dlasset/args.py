fdd113a3220e0c25f61b76e1d51167a0292fd7ef
RaenonX <raenonx0710@gmail.com>
2021-09-18 16:55:54 -0500

    Added env vars support for secrets #15
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- dlasset/args.py
+++ dlasset/args.py
@@ -1,5 +1,6 @@
 """Implementations for parsing the CLI arguments."""
 import argparse
+import os
 from dataclasses import dataclass
 
 __all__ = ("get_cli_args", "CliArgs")
@@ -14,6 +15,7 @@ class CliArgs:
     version_code: str
     iv: str
     key: str
+    buffer: str
     config_path: str
 
 
@@ -22,14 +24,17 @@ def get_cli_args() -> CliArgs:
     parser = argparse.ArgumentParser(description="Downloads and pre-processes Dragalia Lost assets.")
 
     parser.add_argument("version", help="Manifest version code")
-    parser.add_argument("-iv", "--iv", help="IV to decrypt the manifest file", type=str, required=True)
-    parser.add_argument("-key", "--key", help="Key to decrypt the manifest file", type=str, required=True)
-    parser.add_argument("-c", "--config", help="Config file path to use", type=str, required=True)
+    parser.add_argument("-iv", "--iv", help="IV to decrypt the manifest asset", type=str)
+    parser.add_argument("-key", "--key", help="Key to decrypt the manifest asset", type=str)
+    parser.add_argument("-b", "--buffer", help="Buffer to decrypt the manifest asset", type=str)
+    parser.add_argument("-c", "--config", help="Config file path to use", type=str)
 
     args = parser.parse_args()
 
     return CliArgs(
         version_code=cast(str, args.version),
-        iv=cast(str, args.iv), key=cast(str, args.key),
+        iv=cast(str, args.iv or os.environ["CRYPTO_IV"]),
+        key=cast(str, args.key or os.environ["CRYPTO_KEY"]),
+        buffer=cast(str, args.buffer or os.environ["CRYPTO_BUFFER"]),
         config_path=cast(str, args.config)
     )
