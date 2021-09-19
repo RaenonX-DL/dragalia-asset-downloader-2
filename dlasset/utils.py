0bdc44a2ea5fc01c7dc5ffe31ee1616e2a8a2678
RaenonX <raenonx0710@gmail.com>
2021-09-18 20:20:58 -0500

    Added `key_of_call` for concurrent tasks for obtaining results
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- dlasset/utils.py
+++ dlasset/utils.py
@@ -1,19 +1,50 @@
 """Various utility functions."""
 import time
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import Future, ProcessPoolExecutor, as_completed
 from functools import wraps
-from typing import Any, Callable, Sequence, TypeVar
+from typing import Any, Callable, Hashable, Optional, Sequence, TypeVar
 
 from .log import log
 
 __all__ = ("concurrent_run", "time_exec")
 
+K = TypeVar("K", bound=Hashable)
+R = TypeVar("R")
 
-def concurrent_run(fn: Callable, args_list: Sequence[Sequence[Any]]) -> None:
-    """Run ``fn`` concurrently with different set of ``args``."""
+
+def concurrent_run(
+        fn: Callable[[..., Any], R],  # type: ignore
+        args_list: Sequence[Sequence[Any]], /,
+        key_of_call: Optional[Callable[[..., Any], K]] = None  # type: ignore
+) -> Optional[dict[K, R]]:
+    """
+    Run ``fn`` concurrently with different set of ``args``.
+
+    If ``key_of_call`` is not ``None``, a ``dict`` where
+    key is obtained from ``key_of_call`` and the value as the result will be returned.
+    """
+    results: dict[K, R] = {}
+
+    def on_done(key_of_call: Callable[[Any, ...], K], args: Sequence[Any]) -> Callable[[Future], None]:  # type: ignore
+        def inner(future: Future) -> None:
+            results[key_of_call(*args)] = future.result()
+
+        return inner
+
     with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(fn, *args) for args in args_list]
+        futures: list[Future] = []
+        for args in args_list:
+            future = executor.submit(fn, *args)
+
+            if key_of_call:
+                future.add_done_callback(on_done(key_of_call, args))
+
         as_completed(futures)
+
+    if key_of_call:
+        return results
+
+    return None
 
 
 FuncT = TypeVar("FuncT", bound=Callable[..., Any])
