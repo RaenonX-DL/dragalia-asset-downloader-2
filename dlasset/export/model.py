5435cfd8638d86f98d0c304c4e5fc5a9121b3d79
RaenonX <raenonx0710@gmail.com>
2021-09-19 19:16:11 -0500

    Added `ExportInfo` representation for debug
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- dlasset/export/model.py
+++ dlasset/export/model.py
@@ -44,6 +44,9 @@ class ExportInfo:
     def __hash__(self) -> int:
         return hash((self.export_dir, self.asset_name))
 
+    def __repr__(self):
+        return f"{self.asset_name} ({self.container_fallback})"
+
     @property
     def objects(self) -> Iterable[ObjectInfo]:
         """Get the objects to export."""
