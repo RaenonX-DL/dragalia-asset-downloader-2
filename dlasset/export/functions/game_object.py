0aa103144b041d3b5a6ecf59ce55fc7beff93ff9
RaenonX <raenonx0710@gmail.com>
2021-09-20 00:12:40 -0500

    Refactored game object export function
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- dlasset/export/functions/game_object.py
+++ dlasset/export/functions/game_object.py
@@ -7,13 +7,49 @@ from dlasset.log import log
 from dlasset.utils import export_json
 
 if TYPE_CHECKING:
-    from dlasset.export import ExportInfo
+    from dlasset.export import ExportInfo, ObjectInfo
 
 __all__ = ("export_game_object",)
 
 
+def export_single_game_obj(export_info: "ExportInfo", game_obj_info: "ObjectInfo") -> None:
+    """Export a single game object."""
+    tree_export: MonoBehaviourTree = {}
+
+    object_tree = game_obj_info.obj.read_typetree()
+
+    components = [
+        export_info.get_obj_info(component["component"]["m_PathID"]).obj
+        for component in object_tree["m_Component"][1:]  # 1st component is always a `Transform` which is omitted
+    ]
+
+    tree_name = object_tree["m_Name"]
+    tree_components = []
+    for component in components:
+        component_tree = component.read_typetree()
+
+        script_path_id = component_tree["m_Script"]["m_PathID"]
+        if script_path_id:
+            # Attach script type name if available
+            attachment = {
+                "$Script": export_info.get_obj_info(component_tree["m_Script"]["m_PathID"]).obj.name
+            }
+        else:
+            # Otherwise, attach component name
+            attachment = {"$Name": component_tree["m_Name"]}
+
+        tree_components.append(attachment | component_tree)
+
+    tree_export["Name"] = tree_name
+    tree_export["Components"] = tree_components
+
+    export_path: str = os.path.join(export_info.get_export_dir_of_obj(game_obj_info), f"{tree_name}.prefab.json")
+
+    export_json(export_path, tree_export)
+
+
 def export_game_object(export_info: "ExportInfo") -> None:
-    """Export components in ``export_info`` info a single script."""
+    """Export components in ``export_info`` info a single script for each game object."""
     game_obj_info_list = [obj_info for obj_info in export_info.objects if obj_info.obj.type == "GameObject"]
 
     if not game_obj_info_list:
@@ -21,38 +57,7 @@ def export_game_object(export_info: "ExportInfo") -> None:
         return
 
     for idx, game_obj_info in enumerate(game_obj_info_list):
-        tree_export: MonoBehaviourTree = {}
-
-        object_tree = game_obj_info.obj.read_typetree()
-
-        components = [
-            export_info.get_obj_info(component["component"]["m_PathID"]).obj
-            for component in object_tree["m_Component"][1:]  # 1st component is always a `Transform` which is omitted
-        ]
-
-        tree_name = object_tree["m_Name"]
-        tree_components = []
-        for component in components:
-            component_tree = component.read_typetree()
-
-            script_path_id = component_tree["m_Script"]["m_PathID"]
-            if script_path_id:
-                # Attach script type name if available
-                attachment = {
-                    "$Script": export_info.get_obj_info(component_tree["m_Script"]["m_PathID"]).obj.name
-                }
-            else:
-                # Otherwise, attach component name
-                attachment = {"$Name": component_tree["m_Name"]}
-
-            tree_components.append(attachment | component_tree)
-
-        tree_export["Name"] = tree_name
-        tree_export["Components"] = tree_components
-
-        export_path: str = os.path.join(export_info.get_export_dir_of_obj(game_obj_info), f"{tree_name}.prefab.json")
-
-        export_json(export_path, tree_export)
+        export_single_game_obj(export_info, game_obj_info)
 
         if idx % 50 == 0:
             log(
