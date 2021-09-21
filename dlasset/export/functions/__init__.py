135b1549c01dd5c2d5caa91d4f7e5f50ca22d510
RaenonX <raenonx0710@gmail.com>
2021-09-21 01:20:55 -0500

    Added `AnimatorOverrideController` exporting #39
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- dlasset/export/functions/__init__.py
+++ dlasset/export/functions/__init__.py
@@ -1,5 +1,6 @@
 """Functions to export the asset objects."""
 from .anim_ctrl import export_anim_ctrl
+from .anim_override import export_anim_override
 from .game_object import export_game_object
 from .image import export_image
 from .image_alpha import export_image_alpha
--- dlasset/export/lookup.py
+++ dlasset/export/lookup.py
@@ -1,8 +1,8 @@
 """Exporting function index."""
 from dlasset.config import ExportType, UnityType
 from .functions import (
-    export_anim_ctrl, export_game_object, export_image, export_image_alpha, export_image_story, export_mono_behaviour,
-    export_sprite_ui,
+    export_anim_ctrl, export_anim_override, export_game_object, export_image, export_image_alpha,
+    export_image_story, export_mono_behaviour, export_sprite_ui,
 )
 from .types import ExportFunction
 
@@ -16,7 +16,7 @@ EXPORT_FUNCTIONS: dict[ExportType, ExportFunction] = {
     "Sprite-UI": export_sprite_ui,
     "GameObject": export_game_object,
     "AnimatorController": export_anim_ctrl,
-    "AnimatorOverrideController": export_mono_behaviour,
+    "AnimatorOverrideController": export_anim_override,
 }
 
 TYPES_TO_INCLUDE: dict[ExportType, tuple[UnityType, ...]] = {
